8a20f32671cb48dbcffcb2bb3bc8220285b67e92
Zavorotyuk <v.zavorotyuk@gmail.com>
2017-01-29 01:18:12 +0200

    Added few new tasks and pluggins

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,33 @@
 var gulp = require('gulp');
 var del = require('del');
 var htmlreplace = require('gulp-html-replace');
+var runSequence = require('run-sequence');
 
-gulp.task('default', function() {
-  gulp.src('src/esco/index.html')
-    .pipe(htmlreplace({
-        'js': 'js/bundle.min.js'
-    }))
-    .pipe(gulp.dest('_attachments/'));
-});
-////////////
 //Clean ./_attachments folder
-///////////
 
 gulp.task('clean', function() {
   return del('_attachments/*')
 })
 
-///////////
 //Copy all files from ./src to _attachments
-//////////
 
 gulp.task('dev:cp', function() {
        gulp.src(['src/**/*'])
 	    .pipe(gulp.dest('_attachments'))
 });
 
-///////////
 //Copy all files from ./src to _attachments
-//////////
 
-gulp.task('devel', function() {
-
+gulp.task('build:devel', function(callback) {
+  runSequence('clean', 'dev:cp', function() {
+    console.log("Configured development environment");
+  });
 });
 
-///////////
 //Copy all files from ./src to _attachments
-//////////
 
 gulp.task('production', function() {
 
 });
+
+//Change scripts in html pages
