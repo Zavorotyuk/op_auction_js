ef48bf0f9ff691e5b26848c197ac67149cf1819e
Zavorotyuk <v.zavorotyuk@gmail.com>
2017-01-28 22:22:23 +0200

    Added some tasks for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,43 @@
 var gulp = require('gulp');
+var del = require('del');
+var htmlreplace = require('gulp-html-replace');
+
+gulp.task('default', function() {
+  gulp.src('src/esco/index.html')
+    .pipe(htmlreplace({
+        'js': 'js/bundle.min.js'
+    }))
+    .pipe(gulp.dest('_attachments/'));
+});
+////////////
+//Clean ./_attachments folder
+///////////
+
+gulp.task('clean', function() {
+  return del('_attachments/*')
+})
+
+///////////
+//Copy all files from ./src to _attachments
+//////////
+
+gulp.task('dev:cp', function() {
+       gulp.src(['src/**/*'])
+	    .pipe(gulp.dest('_attachments'))
+});
+
+///////////
+//Copy all files from ./src to _attachments
+//////////
+
+gulp.task('devel', function() {
+
+});
+
+///////////
+//Copy all files from ./src to _attachments
+//////////
+
+gulp.task('production', function() {
+
+});
