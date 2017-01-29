e5d74a18411acf6dfe08d0f3a8b83af5c4e19786
Zavorotyuk <v.zavorotyuk@gmail.com>
2017-01-29 13:54:56 +0200

    Updated gulpfile, added few pluggins

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var del = require('del');
 var htmlreplace = require('gulp-html-replace');
 var runSequence = require('run-sequence');
+var concat = require('gulp-concat');
 
 //Clean ./_attachments folder
 
@@ -26,8 +27,41 @@ gulp.task('build:devel', function(callback) {
 
 //Copy all files from ./src to _attachments
 
-gulp.task('production', function() {
+gulp.task('build:production', function() {
 
 });
 
-//Change scripts in html pages
+//Change scripts in esco/index.html pages
+
+gulp.task('esco:html', function() {
+  gulp.src('src/esco/index.html')
+    .pipe(htmlreplace({
+        'js': 'static/js/main.min.js',
+        'vendors': 'static/vendors/main.min.js'
+    }))
+    .pipe(gulp.dest('_attachments/esco/'));
+});
+
+//Change scripts in tenders/index.html
+gulp.task('tenders:html', function() {
+  gulp.src('src/tenders/index.html')
+    .pipe(htmlreplace({
+        'js': 'static/js/main.min.js',
+        'vendors': 'static/vendors/main.min.js'
+    }))
+    .pipe(gulp.dest('_attachments/tenders/'));
+});
+
+
+// Replace html
+
+gulp.task('replace:html', ['esco:html', 'tenders:html']);
+
+
+//concat js
+
+gulp.task('scripts:concat', function() {
+  return gulp.src('src/static/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('_attachments/static/js'));
+});
