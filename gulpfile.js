9f18f1dbda01086169826f134426a096246317ab
Volodymyr Zavorotyuk <zavorotyuk@quintagroup.org>
2017-01-31 16:23:42 +0200

    Code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -15,47 +15,42 @@ var ngAnnotate = require('gulp-ng-annotate');
 var plumber = require('gulp-plumber');
 
 //watch
-  gulp.task('watch:devel', function() {
-    gulp.watch('./**/src/**/*', ['build:devel'])
-  });
+  gulp.task('watch:devel', () => gulp.watch('./**/src/**/*', ['build:devel']) );
 
 
 // common
-  gulp.task('clean', function() {
-    return gulp.src('./_attachments', {read: false})
-      .pipe(clean());
-  })
+  gulp.task('clean', () =>
+    gulp.src('./_attachments', {read: false})
+      .pipe(clean())
+  );
 
 
 //devel
 
-gulp.task('build:devel',function(callback){
-  return runSequence(
-      'clean','dev:cp',  callback
+  gulp.task('build:devel',(callback) =>
+    runSequence('clean','dev:cp',  callback)
   );
-});
 
-  gulp.task('dev:cp', function() {
-    return gulp.src(['src/**/*'])
+  gulp.task('dev:cp', () =>
+    gulp.src(['src/**/*'])
       .pipe(gulp.dest('./_attachments/'))
-  });
+  );
 
 
 //production
 
-  gulp.task('build:production', function() {
+  gulp.task('build:production', () =>
     runSequence('clean', ['replace:html', 'prepare:static',
       'copy:vendors', 'minify:js','minify:img','minify:main.css',
-      'copy:other'],
-       function() {
-         console.log('Configured production environment');
+      'copy:other'], () => {
+        console.log("Configured production environment");
       })
-  });
+  );
 
 
   gulp.task('replace:html', ['replace:esco', 'replace:tenders']);
 
-  gulp.task('replace:esco', function() {
+  gulp.task('replace:esco', () =>
     gulp.src('src/esco/index.html')
       .pipe(plumber())
       .pipe(htmlreplace({
@@ -64,10 +59,10 @@ gulp.task('build:devel',function(callback){
           'css': '../static/css/starter-template.min.css'
       }))
       .pipe(plumber.stop())
-      .pipe(gulp.dest('_attachments/esco/'));
-  });
+      .pipe(gulp.dest('_attachments/esco/'))
+  );
 
-  gulp.task('replace:tenders', function() {
+  gulp.task('replace:tenders', () =>
     gulp.src('src/tenders/index.html')
       .pipe(plumber())
       .pipe(htmlreplace({
@@ -76,15 +71,15 @@ gulp.task('build:devel',function(callback){
           'css': '../static/css/starter-template.min.css'
       }))
       .pipe(plumber.stop())
-      .pipe(gulp.dest('_attachments/tenders/'));
-  });
+      .pipe(gulp.dest('_attachments/tenders/'))
+  );
 
 
   gulp.task('prepare:static', ['minify:js','minify:static/css',
     'minify:static/img','copy:fonts']);
 
 
-  gulp.task('minify:js', function () {
+  gulp.task('minify:js', () =>
     gulp.src(['src/static/js/escoModule.js', ,'src/static/js/app.js', 'src/static/js/*.js'])
       .pipe(plumber())
       .pipe(concat('main.min.js'))
@@ -92,41 +87,41 @@ gulp.task('build:devel',function(callback){
       .pipe(uglify())
       .pipe(plumber.stop())
       .pipe(gulp.dest('_attachments/static/js'))
-  })
+  );
 
 
-  gulp.task('minify:static/css', function() {
-    return gulp.src('src/static/css/*.css')
+  gulp.task('minify:static/css', () =>
+    gulp.src('src/static/css/*.css')
       .pipe(plumber())
       .pipe(cleanCss({compatibility: 'ie8'}))
       .pipe(rename('starter-template.min.css'))
       .pipe(plumber.stop())
-      .pipe(gulp.dest('_attachments/static/css'));
-  });
+      .pipe(gulp.dest('_attachments/static/css'))
+  );
 
 
-  gulp.task('minify:static/img', function() {
+  gulp.task('minify:static/img', () =>
     gulp.src('src/static/img/*')
     .pipe(plumber())
     .pipe(imagemin())
     .pipe(plumber.stop())
     .pipe(gulp.dest('_attachments/static/img'))
-  })
+  );
 
 
-  gulp.task('copy:fonts', function() {
+  gulp.task('copy:fonts', () =>
     gulp.src('src/static/fonts/*')
     .pipe(gulp.dest('_attachments/static/fonts'))
-  })
+  );
 
 
-  gulp.task('minify:main.css', function() {
+  gulp.task('minify:main.css', () =>
     gulp.src('src/style/main.css')
     .pipe(plumber())
     .pipe(cleanCss({compatibility: 'ie8'}))
     .pipe(plumber.stop())
     .pipe(gulp.dest('_attachments/style/'))
-  })
+  );
 
 
   gulp.task('minify:img', () =>
@@ -138,13 +133,13 @@ gulp.task('build:devel',function(callback){
   );
 
 
-  gulp.task('copy:vendors', function() {
+  gulp.task('copy:vendors', () =>
     gulp.src('src/vendor/**/*')
     .pipe(gulp.dest('_attachments/vendor'))
-  })
+  );
 
 
-  gulp.task('copy:other', function() {
+  gulp.task('copy:other', () =>
     gulp.src(['src/*.xml','src/*.ico','src/*.json','src/get_current_server_time'])
     .pipe(gulp.dest('_attachments'))
-  })
+  );
